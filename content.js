b4421b85a334cf5cd9784de8cc3c11556aa425db
yudai-tiny-developer <yudai-tiny-developer@gmail.com>
2024-06-05 02:25:21 +0900

    span

--- content.js
+++ content.js
@@ -20,28 +20,28 @@ function main(common) {
     }
 
     chrome.storage.onChanged.addListener(() => {
-        document.querySelectorAll('button._tap_volume_button').forEach(b => b.remove());
+        document.querySelectorAll('span._tap_volume_button').forEach(b => b.remove());
         apply_settings(true);
     });
 
     function apply_settings(force = false) {
         chrome.storage.local.get(common.storage, data => {
-            create_volume_buttons(data, force);
+            create_buttons(data, force);
             set_slider_display(data);
         });
     }
 
-    function create_volume_buttons(data, force) {
-        const volume_area = document.querySelector('span.ytp-volume-area');
-        if (volume_area && (force || !volume_area.getAttribute('_tap_volume'))) {
-            volume_area.setAttribute('_tap_volume', true);
-            const panel = volume_area.querySelector('div.ytp-volume-panel');
+    function create_buttons(data, force) {
+        const area = document.querySelector('span.ytp-volume-area');
+        if (area && (force || !area.getAttribute('_tap_volume'))) {
+            area.setAttribute('_tap_volume', true);
+            const panel = area.querySelector('div.ytp-volume-panel');
 
-            if (common.value(data.v1_enabled, common.default_v1_enabled)) { create_volume_button(common.value(data.v1, common.default_v1), volume_area, panel); }
-            if (common.value(data.v2_enabled, common.default_v2_enabled)) { create_volume_button(common.value(data.v2, common.default_v2), volume_area, panel); }
-            if (common.value(data.v3_enabled, common.default_v3_enabled)) { create_volume_button(common.value(data.v3, common.default_v3), volume_area, panel); }
-            if (common.value(data.v4_enabled, common.default_v4_enabled)) { create_volume_button(common.value(data.v4, common.default_v4), volume_area, panel); }
-            if (common.value(data.v5_enabled, common.default_v5_enabled)) { create_volume_button(common.value(data.v5, common.default_v5), volume_area, panel); }
+            if (common.value(data.v1_enabled, common.default_v1_enabled)) { create_button(common.value(data.v1, common.default_v1), area, panel); }
+            if (common.value(data.v2_enabled, common.default_v2_enabled)) { create_button(common.value(data.v2, common.default_v2), area, panel); }
+            if (common.value(data.v3_enabled, common.default_v3_enabled)) { create_button(common.value(data.v3, common.default_v3), area, panel); }
+            if (common.value(data.v4_enabled, common.default_v4_enabled)) { create_button(common.value(data.v4, common.default_v4), area, panel); }
+            if (common.value(data.v5_enabled, common.default_v5_enabled)) { create_button(common.value(data.v5, common.default_v5), area, panel); }
         }
     }
 
@@ -53,17 +53,22 @@ function main(common) {
         }
     }
 
-    function create_volume_button(volume, volume_area, panel) {
-        const volume_button = document.createElement('button');
-        volume_button.title = volume + '%';
-        volume_button.innerText = volume_button.title;
-        volume_button.classList.add('_tap_volume_button', 'ytp-button');
-        volume_button.setAttribute('tabindex', '-1');
-        volume_button.addEventListener('click', () => {
-            document.dispatchEvent(new CustomEvent('_tap_volume', { detail: volume }));
-            volume_button.blur();
+    function create_button(value, area, panel) {
+        const button = document.createElement('button');
+        button.title = value + '%';
+        button.innerText = button.title;
+        button.classList.add('_tap_volume_button', 'ytp-button');
+        button.setAttribute('tabindex', '-1');
+        button.addEventListener('click', () => {
+            document.dispatchEvent(new CustomEvent('_tap_volume', { detail: value }));
+            button.blur();
         });
-        volume_area.insertBefore(volume_button, panel);
+
+        const span = document.createElement('span');
+        span.classList.add('_tap_volume_button', 'ytp-button');
+        span.appendChild(button);
+
+        area.insertBefore(span, panel);
     }
 
     const s = document.createElement('script');
