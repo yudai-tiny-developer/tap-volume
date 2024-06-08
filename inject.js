1c3913a79a9b9adeabecdc55affe3ba76535706e
yudai-tiny-developer <yudai-tiny-developer@gmail.com>
2024-06-08 20:05:40 +0900

    redesign

--- inject.js
+++ inject.js
@@ -1,27 +1,37 @@
-function _tap_volume_remove_class(selector, value) {
-    for (const button of document.body.querySelectorAll(selector)) {
-        button.classList.remove(value);
-    }
-}
-
-function _tap_volume_add_class(selector, value) {
-    for (const button of document.body.querySelectorAll(selector)) {
-        button.classList.add(value);
+function _tap_volume_update_class(remove_class, add_target_class, add_value) {
+    for (const button of document.body.querySelectorAll('button._tap_volume_button')) {
+        const oldToken = button.classList.contains(remove_class) ? remove_class : undefined;
+        const newToken = button.classList.contains(add_target_class) ? add_value : undefined;
+        if (oldToken && newToken) {
+            button.classList.replace(oldToken, newToken);
+        } else if (oldToken) {
+            button.classList.remove(oldToken);
+        } else if (newToken) {
+            button.classList.add(newToken);
+        }
     }
 }
 
 function _tap_volume_activate(value) {
-    _tap_volume_remove_class('button._tap_volume_active', '_tap_volume_active');
-    _tap_volume_add_class('button._tap_volume_button_' + value, '_tap_volume_active');
-
-    if (!document.body.querySelector('button._tap_volume_active')) {
-        _tap_volume_add_class('button._tap_volume_tap', '_tap_volume_active');
+    if (document.body.querySelector('button._tap_volume_button._tap_volume_button_' + value)) {
+        _tap_volume_update_class('_tap_volume_active', '_tap_volume_button_' + value, '_tap_volume_active')
+    } else {
+        _tap_volume_update_class('_tap_volume_active', '_tap_volume_tap', '_tap_volume_active')
     }
 }
 
+function _tap_volume_init(e) {
+    _tap_volume_activate(e.volume);
+}
+
+document.addEventListener('_tap_volume_init', e => {
+    const player = document.body.querySelector('div#movie_player');
+    _tap_volume_activate(player.getVolume());
+    player.addEventListener('onVolumeChange', _tap_volume_init);
+});
+
 document.addEventListener('_tap_volume', e => {
-    _tap_volume_remove_class('button._tap_volume_tap', '_tap_volume_tap');
-    _tap_volume_add_class('button._tap_volume_button_' + e.detail, '_tap_volume_tap');
+    _tap_volume_update_class('_tap_volume_tap', '_tap_volume_button_' + e.detail, '_tap_volume_tap');
 
     const player = document.body.querySelector('div#movie_player');
     if (e.detail === 0) {
@@ -30,18 +40,4 @@ document.addEventListener('_tap_volume', e => {
         player.setVolume(e.detail);
         player.unMute();
     }
-    _tap_volume_activate(player.getVolume());
-});
-
-document.addEventListener('_tap_volume_init', e => {
-    const player = document.body.querySelector('div#movie_player');
-    _tap_volume_activate(player.getVolume());
-    player.addEventListener('onVolumeChange', e => {
-        _tap_volume_activate(e.volume);
-    });
-});
-
-document.addEventListener('_tap_volume_update', e => {
-    const player = document.body.querySelector('div#movie_player');
-    _tap_volume_activate(player.getVolume());
 });
