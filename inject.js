41ffc7cf510b0516845d64ea76a7689dd6067d96
Simple and Tiny Utilities <108601122+yudai-tiny-developer@users.noreply.github.com>
2024-06-09 02:36:34 +0900

    v1.4.5 (#11)
    Co-authored-by: yudai-tiny-developer <yudai-tiny-developer@gmail.com>

--- inject.js
+++ inject.js
@@ -16,29 +16,31 @@ function _tap_volume_activate(value) {
     _tap_volume_update_class('_tap_volume_active', '_tap_volume_button_' + value, '_tap_volume_active');
 }
 
-function _tap_volume_onChange(e) {
-    _tap_volume_activate(e.volume);
-}
-
 let _tap_volume_init = true;
 
 document.addEventListener('_tap_volume_init', e => {
     const player = document.body.querySelector('div#movie_player');
-    _tap_volume_activate(player.getVolume());
-    if (_tap_volume_init) {
-        _tap_volume_init = false;
-        player.addEventListener('onVolumeChange', _tap_volume_onChange, { once: true });
+    if (player) {
+        _tap_volume_activate(player.getVolume());
+        if (_tap_volume_init) {
+            _tap_volume_init = false;
+            player.addEventListener('onVolumeChange', e => {
+                _tap_volume_activate(e.volume);
+            });
+        }
     }
 });
 
 document.addEventListener('_tap_volume', e => {
     const player = document.body.querySelector('div#movie_player');
-    if (e.detail === 0) {
-        player.mute();
-    } else {
-        player.setVolume(e.detail);
-        if (player.isMuted()) {
-            player.unMute();
+    if (player) {
+        if (e.detail === 0) {
+            player.mute();
+        } else {
+            player.setVolume(e.detail);
+            if (player.isMuted()) {
+                player.unMute();
+            }
         }
     }
 });
