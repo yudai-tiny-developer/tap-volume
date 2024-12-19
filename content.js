d94d337035623284e0f85f2d7428dc7e3010383e
yudai-tiny-developer <yudai-tiny-developer@gmail.com>
2024-12-19 15:42:27 +0900

    v1.8.4

--- content.js
+++ content.js
@@ -15,14 +15,13 @@ function main(app, common) {
 
     function create_buttons(data) {
         const area = app.querySelector('span.ytp-volume-area');
-        if (area && !area.querySelector('button._tap_volume_button')) {
-            const panel = area.querySelector('div.ytp-volume-panel');
-
-            if (common.value(data.v1_enabled, common.default_v1_enabled)) { create_button(common.value(data.v1, common.default_v1), area, panel); }
-            if (common.value(data.v2_enabled, common.default_v2_enabled)) { create_button(common.value(data.v2, common.default_v2), area, panel); }
-            if (common.value(data.v3_enabled, common.default_v3_enabled)) { create_button(common.value(data.v3, common.default_v3), area, panel); }
-            if (common.value(data.v4_enabled, common.default_v4_enabled)) { create_button(common.value(data.v4, common.default_v4), area, panel); }
-            if (common.value(data.v5_enabled, common.default_v5_enabled)) { create_button(common.value(data.v5, common.default_v5), area, panel); }
+        if (area) {
+            let panel = area.querySelector('button.ytp-settings-button');
+            panel = update_button(data.v5, common.default_v5, area, panel, data.v5_enabled, common.default_v5_enabled);
+            panel = update_button(data.v4, common.default_v4, area, panel, data.v4_enabled, common.default_v4_enabled);
+            panel = update_button(data.v3, common.default_v3, area, panel, data.v3_enabled, common.default_v3_enabled);
+            panel = update_button(data.v2, common.default_v2, area, panel, data.v2_enabled, common.default_v2_enabled);
+            panel = update_button(data.v1, common.default_v1, area, panel, data.v1_enabled, common.default_v1_enabled);
         }
     }
 
@@ -34,8 +33,16 @@ function main(app, common) {
         }
     }
 
+    function update_button(data, default_value, area, panel, enabled, default_enabled) {
+        const value = common.value(data, default_value);
+        const button = area.querySelector('button._tap_volume_button_' + value) ?? create_button(common.value(data, default_value), area, panel);
+        button.style.display = common.value(enabled, default_enabled) ? '' : 'none';
+        return button;
+    }
+
     function create_button(value, area, panel) {
         const button = document.createElement('button');
+        button.style.display = 'none';
         button.innerHTML = `<svg width="100%" height="100%" viewBox="0 0 72 72"><text font-size="20" x="50%" y="50%" dominant-baseline="middle" text-anchor="middle">${value}%</text></svg>`;
         button.classList.add('_tap_volume_button', '_tap_volume_button_' + value, 'ytp-button');
         button.addEventListener('click', () => {
@@ -43,6 +50,7 @@ function main(app, common) {
             button.blur();
         });
         area.insertBefore(button, panel);
+        return button;
     }
 
     document.addEventListener('_tap_volume_init', e => {
