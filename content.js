0e49f5aef8786a22bb8184bc6849f0c1df5682c2
yudai-tiny-developer <yudai-tiny-developer@gmail.com>
2025-02-28 03:06:22 +0900

    content

--- content.js
+++ content.js
@@ -6,72 +6,48 @@ import(chrome.runtime.getURL('common.js')).then(common => {
 
 function main(app, common) {
     function loadSettings() {
-        const area = app.querySelector('span.ytp-volume-area');
-        if (!area) {
-            return false;
-        }
-
-        const panel = area.querySelector('div.ytp-volume-panel');
-        if (!panel) {
-            return false;
-        }
-
         chrome.storage.local.get(common.storage, data => {
-            update_buttons(data, area, panel);
-            update_slider(data);
-            update_shortcut_command(data);
+            settings = data;
+            update_buttons();
+            update_slider();
             document.dispatchEvent(new CustomEvent('_tap_volume_loaded'));
         });
-
-        return true;
     }
 
-    function update_buttons(data, area, panel) {
-        const buttons = area.querySelectorAll('button._tap_volume_button');
-        if (buttons.length === 0) {
-            const buttonsSet = new Set(buttons);
-
-            panel = update_button(data.v5, common.default_v5, area, panel, data.v5_enabled, common.default_v5_enabled); buttonsSet.delete(panel);
-            panel = update_button(data.v4, common.default_v4, area, panel, data.v4_enabled, common.default_v4_enabled); buttonsSet.delete(panel);
-            panel = update_button(data.v3, common.default_v3, area, panel, data.v3_enabled, common.default_v3_enabled); buttonsSet.delete(panel);
-            panel = update_button(data.v2, common.default_v2, area, panel, data.v2_enabled, common.default_v2_enabled); buttonsSet.delete(panel);
-            panel = update_button(data.v1, common.default_v1, area, panel, data.v1_enabled, common.default_v1_enabled); buttonsSet.delete(panel);
-
-            for (const button of buttonsSet) {
-                button.remove();
-            }
+    function update_buttons() {
+        if (settings) {
+            update_button(button_v1, settings.v1, common.default_v1, settings.v1_enabled, common.default_v1_enabled);
+            update_button(button_v2, settings.v2, common.default_v2, settings.v2_enabled, common.default_v2_enabled);
+            update_button(button_v3, settings.v3, common.default_v3, settings.v3_enabled, common.default_v3_enabled);
+            update_button(button_v4, settings.v4, common.default_v4, settings.v4_enabled, common.default_v4_enabled);
+            update_button(button_v5, settings.v5, common.default_v5, settings.v5_enabled, common.default_v5_enabled);
         }
     }
 
-    function update_slider(data) {
-        if (common.value(data.hide_slider, common.default_hide_slider)) {
-            document.documentElement.style.setProperty('--tap-volume-slider-display', 'none');
-        } else {
-            document.documentElement.style.setProperty('--tap-volume-slider-display', 'block');
-        }
-    }
-
-    function update_button(data, default_value, area, panel, enabled, default_enabled) {
-        const value = common.value(data, default_value);
-        const button = area.querySelector('button._tap_volume_button_' + value) ?? create_button(common.value(data, default_value), area, panel);
+    function update_button(button, value, default_value, enabled, default_enabled) {
+        const detail = common.value(value, default_value);
         button.style.display = common.value(enabled, default_enabled) ? '' : 'none';
-        return button;
+        button.classList.add('_tap_volume_button', '_tap_volume_button_' + detail, 'ytp-button');
+        button.innerHTML = `<svg width="100%" height="100%" viewBox="0 0 72 72"><text font-size="20" x="50%" y="50%" dominant-baseline="central" text-anchor="middle">${detail}%</text></svg>`;
+        button.addEventListener('click', () => {
+            document.dispatchEvent(new CustomEvent('_tap_volume', { detail: detail }));
+        });
     }
 
-    function create_button(value, area, panel) {
+    function update_slider() {
+        if (settings) {
+            document.documentElement.style.setProperty('--tap-volume-slider-display', common.value(settings.hide_slider, common.default_hide_slider) ? 'none' : 'block');
+        }
+    }
+
+    function create_button() {
         const button = document.createElement('button');
         button.style.display = 'none';
-        button.innerHTML = `<svg width="100%" height="100%" viewBox="0 0 72 72"><text font-size="20" x="50%" y="50%" dominant-baseline="central" text-anchor="middle">${value}%</text></svg>`;
-        button.classList.add('_tap_volume_button', '_tap_volume_button_' + value, 'ytp-button');
-        button.addEventListener('click', () => {
-            document.dispatchEvent(new CustomEvent('_tap_volume', { detail: value }));
-        });
-        area.insertBefore(button, panel);
         return button;
     }
 
-    function update_shortcut_command(data) {
-        shortcut_command = command => {
+    const shortcut_command = command => {
+        if (settings) {
             let value;
             switch (command) {
                 case 'v1':
@@ -93,22 +69,42 @@ function main(app, common) {
                     return;
             }
             document.dispatchEvent(new CustomEvent('_tap_volume', { detail: value }));
-        };
-    }
-
-    let shortcut_command;
-    chrome.runtime.onMessage.addListener(command => {
-        if (shortcut_command) {
-            shortcut_command(command);
         }
-    });
+    };
+
+    const button_v1 = create_button();
+    const button_v2 = create_button();
+    const button_v3 = create_button();
+    const button_v4 = create_button();
+    const button_v5 = create_button();
+
+    let settings;
+
+    chrome.runtime.onMessage.addListener(shortcut_command);
 
     chrome.storage.onChanged.addListener(loadSettings);
+
     document.addEventListener('_tap_volume_init', e => {
-        const interval = setInterval(() => {
-            if (loadSettings()) {
-                clearInterval(interval);
+        const detect_interval = setInterval(() => {
+            const area = app.querySelector('span.ytp-volume-area');
+            if (!area) {
+                return false;
             }
+
+            const panel = area.querySelector('div.ytp-volume-panel');
+            if (!panel) {
+                return false;
+            }
+
+            clearInterval(detect_interval);
+
+            area.insertBefore(button_v5, panel);
+            area.insertBefore(button_v4, button_v5);
+            area.insertBefore(button_v3, button_v4);
+            area.insertBefore(button_v2, button_v3);
+            area.insertBefore(button_v1, button_v2);
+
+            loadSettings();
         }, 200);
     });
 
